20fcfaf4b02f0cb7f2ea76976e041bf16f69b01f
kitazaki <kitazaki@gmail.com>
2020-05-10 14:49:42 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -95,6 +95,40 @@ class Connpass {
         });
     }
 
+    // 検索イベントリスト表示
+    getSearchList(){
+        let page_num = 1;
+        return axios.get(this.group_url).then(res => {
+            const d = res.data.match(/  <h2 class="main_h2">検索結果 \((.*?)件\)<\/h2>/)[1];
+            // console.log(d);
+            if (Number(d) > 0) {
+                if (Number(d) > 10) {
+                    page_num = Math.floor(Number(d) / 10) + 1;
+                }
+                for (let i=1; i <= page_num; i++){
+                    this._getSearchList(i);
+                }
+            }
+            return Number(d);
+        });
+    }
+
+    _getSearchList(page){
+        return axios.get(`${this.group_url}&page=${page}`).then(res => {
+            const lines = res.data.split(/\n/);
+            const size = lines.length;
+            const maxWidth = size.toString().length;
+            lines
+                .map((line, index) => [(index + 1).toString().padStart(maxWidth, " "), line])
+                .filter(lineWithIndex => lineWithIndex[1].search(`event_title`) > -1)
+                .forEach(lineWithIndex => {
+                    const name = lineWithIndex[1].match(/      <p class="event_title"><a class="url summary" href=".*?">(.*?)<\/a><\/p>/)[1];
+                    const url = lineWithIndex[1].match(/      <p class="event_title"><a class="url summary" href="(.*?)">.*?<\/a><\/p>/)[1];
+                    console.log(`${name}, ${url}`);
+                });
+        });
+    }
+
     // メソッド area()
     area() { return this.width * this.height; }
 
