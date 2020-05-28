74f66eaec05ef3509a533ab5ad1bf9fe5e48ce6b
n0bisuke <rsksound@gmail.com>
2020-05-28 22:53:04 +0900

    Minor Update

--- src/_getAllEventPageHtml.js
+++ src/_getAllEventPageHtml.js
@@ -6,26 +6,11 @@ let allPageHtml = '';
 
 module.exports = async (group_url, page=1) => {
     let oldPage = '';
-    console.time('timer1');
     while(1){
-        // const res = await axios.get(`${group_url}/event?page=${page}`);
-        // let newPage = res.data
-        //             .replace(/\r?\n/g,"")
-        //             .match(/class="group_inner">(.*?)<div id="side_area"/)[1];
-        const res = await axios.all([
-            axios.get(`${group_url}/event?page=${page}`),
-            axios.get(`${group_url}/event?page=${page++}`)
-        ]);
-         
-        let newPage = res[0].data
-                            .replace(/\r?\n/g,"")
-                            .match(/class="group_inner">(.*?)<div id="side_area"/)[1];
-        console.log(res[1].data);
-        newPage += res[1].data
-                        .replace(/\r?\n/g,"")
-                        .match(/class="group_inner">(.*?)<div id="side_area"/)[1];
-
-
+        const res = await axios.get(`${group_url}/event?page=${page}`);
+        let newPage = res.data
+                    .replace(/\r?\n/g,"")
+                    .match(/class="group_inner">(.*?)<div id="side_area"/)[1];
 
         if(oldPage !== newPage){
             allPageHtml += newPage;
@@ -37,36 +22,6 @@ module.exports = async (group_url, page=1) => {
             break;
         }
     }
-    console.timeEnd('timer1');
+
     return allPageHtml;
 }
-
-// // module.exports = async (group_url, page=1) => {
-// //     // let oldPage = '';
-// //     console.time('timer1');
-// //     for (let i = 1; i < 28; i++) {
-// //         const res = await axios.get(`${group_url}/event?page=${page}`);
-// //         allPageHtml += res.data;
-// //         process.stdout.write('.');
-// //         page++;
-// //     }
-// //     console.timeEnd('timer1');
-// //     // return allPageHtml;
-// // }
-
-
-
-// module.exports = async (group_url, page=1) => {
-//     // let oldPage = '';
-//     console.time('timer1');
-//     let result = [];
-//     for (let i = 1; i < 28; i++) {
-//         result.push(axios.get(`${group_url}/event?page=${page}`));
-//         page++;
-//     }
-    
-//     const res = await Promise.all(result);
-//     console.log(res.data);
-//     console.timeEnd('timer1');
-//     // return allPageHtml;
-// }
