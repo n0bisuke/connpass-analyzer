964a86683fb99a57e94fbd663abfc4beb1fd9e13
kitazaki <kitazaki@gmail.com>
2020-06-03 15:28:03 +0900

    Add files via upload

--- examples/prefecture.js
+++ examples/prefecture.js
@@ -1,6 +1,6 @@
 'use strict';
  
-const Connpass = require('connpass-analyzer');
+const Connpass = require('../index.js');
 const prefectures = ["hokkaido", "aomori", "iwate", "miyagi", "akita", "yamagata", "fukushima", "ibaraki", "tochigi", "gunma", "saitama", "chiba", "tokyo", "kanagawa", "yamanashi", "nagano", "niigata", "toyama", "ishikawa", "fukui", "gifu", "shizuoka", "aichi", "mie", "shiga", "kyoto", "osaka", "hyogo", "nara", "wakayama", "tottori", "shimane", "okayama", "hiroshima", "yamaguchi", "tokushima", "kagawa", "ehime", "kochi", "fukuoka", "saga", "nagasaki", "kumamoto", "oita", "miyazaki", "kagoshima", "okinawa", "online"];
 const i = 47;  // 0 => hokkaido, 1 => aomori, .. , 47 => online
 const start_date = '2020%2F03%2F15';  // start date of search. format is YYYY%2FMM%2FDD. YYYY => year, %2F => URL encoded '/', MM => month, %2F => URL encoded '/', DD => day
